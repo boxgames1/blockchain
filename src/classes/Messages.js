768c447e1ab8981c46c4cbf2c4ed51480e60f55d
Oliver <oliver@xeach.es>
2018-10-05 13:31:46 +0200

    Base classes

---
+++ src/classes/Messages.js
@@ -0,0 +1,40 @@
+import messageType from "../constants/MessageType";
+
+const {
+  REQUEST_LATEST_BLOCK,
+  RECEIVE_LATEST_BLOCK,
+  REQUEST_BLOCKCHAIN,
+  RECEIVE_BLOCKCHAIN,
+  REQUEST_TRANSACTIONS,
+  RECEIVE_TRANSACTIONS
+} = messageType;
+
+class Messages {
+  static getLatestBlock() {
+    return {
+      type: REQUEST_LATEST_BLOCK
+    };
+  }
+
+  static sendLatestBlock(block) {
+    return {
+      type: RECEIVE_LATEST_BLOCK,
+      data: block
+    };
+  }
+
+  static getBlockchain() {
+    return {
+      type: REQUEST_BLOCKCHAIN
+    };
+  }
+
+  static sendBlockchain(blockchain) {
+    return {
+      type: RECEIVE_BLOCKCHAIN,
+      data: blockchain
+    };
+  }
+}
+
+export default Messages;
---
+++ src/constants/MessageType.js
@@ -0,0 +1,7 @@
+const messageType = {
+  REQUEST_LATEST_BLOCK: 0,
+  RECEIVE_LATEST_BLOCK: 1,
+  REQUEST_BLOCKCHAIN: 2,
+  RECEIVE_BLOCKCHAIN: 3
+};
+export default messageType;
